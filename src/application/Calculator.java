49f98dd5297b93f92da9228c6159ec719e9e4e8d
giannis <johntriantos13@hotmail.com>
2017-06-07 18:11:38 +0300

    initial commit

---
+++ src/application/Calculator.java
@@ -0,0 +1,29 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Calculator extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("calculator.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Calculator");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/CalculatorController.java
@@ -0,0 +1,45 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+public class CalculatorController {
+	
+	@FXML private MenuItem about;
+	@FXML private TextField details;
+	@FXML private Button zero;
+	@FXML private Button point;
+	@FXML private Button clear;
+	@FXML private Button one;
+	@FXML private Button two;
+	@FXML private Button three;
+	@FXML private Button four;
+	@FXML private Button five;
+	@FXML private Button six;
+	@FXML private Button seven;
+	@FXML private Button eight;
+	@FXML private Button nine;
+	@FXML private Button del;
+	@FXML private Button paropen;
+	@FXML private Button parclose;
+	@FXML private Button equals;
+	@FXML private Button add;
+	@FXML private Button sub;
+	@FXML private Button mul;
+	@FXML private Button div;
+	
+	@FXML
+	private void buttonsClicked(ActionEvent e) {
+		
+		
+		
+	}
+	
+	@FXML
+	private void menuItemClicked(ActionEvent e) {
+		
+	}
+}
